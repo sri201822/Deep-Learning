99eaa3adc4fcf1c50d720921f419107b9ea42132
Naga Sri Harsha Akavarapu <39739377+sri201822@users.noreply.github.com>
2020-02-13 23:47:56 +0530

    Create Tensorflow_setup_for_windows.py

---
+++ Tensorflow_setup_for_windows.py
@@ -0,0 +1,12 @@
+# Open Anaconda command Prompt and type the following commands in sequence.
+step 1 : conda create --name tensorflow python=3.7 # for python 3.7 version
+step 2 :activate tensorflow
+step 3: pip install tensorflow
+step 4: pip install tensorflow-gpu
+# check for tensorflow
+>> activate tensorflow
+>> python (activating python shell)
+>> import tensorflow as tf
+>> hello = tf.constant(‘Hello, Tensorflow!’)
+>> sess = tf.Session()
+>> print(sess.run(hello))
