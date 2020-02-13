4b0ad0754c0d2e4f65e4574c157927eba43884a1
Naga Sri Harsha Akavarapu <39739377+sri201822@users.noreply.github.com>
2020-02-14 00:24:35 +0530

    Update Tensorflow_setup_for_windows.py

--- Tensorflow_setup_for_windows.py
+++ Tensorflow_setup_for_windows.py
@@ -1,5 +1,5 @@
 # Open Anaconda command Prompt and type the following commands in sequence.
-step 1 : conda create --name tensorflow python=3.6 # for python 3.7 version
+step 1 : conda create --name tensorflow python=3.6 # for python 3.6 version
 step 2 :activate tensorflow
 step 3: pip install tensorflow
 step 4: pip install tensorflow-gpu
