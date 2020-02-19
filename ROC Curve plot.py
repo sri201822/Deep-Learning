c02b8bfee30120d5fe02718a5f9a8e9a39876355
Naga Sri Harsha Akavarapu <39739377+sri201822@users.noreply.github.com>
2020-02-19 23:34:19 +0530

    Create ROC Curve plot.py

---
+++ ROC Curve plot.py
@@ -0,0 +1,14 @@
+def plot_roc(pred,y):
+    fpr, tpr, _ = roc_curve(y, pred)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC)')
+    plt.legend(loc="lower right")
+    plt.show()
