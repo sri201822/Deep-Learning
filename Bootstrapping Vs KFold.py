ea3efbe7f2c06cbe9386f96800bdd639ad2582e4
Naga Sri Harsha Akavarapu <39739377+sri201822@users.noreply.github.com>
2020-02-23 21:08:45 +0530

    Create Bootstrapping Vs KFold.py

---
+++ Bootstrapping Vs KFold.py
@@ -0,0 +1,4 @@
+# Difference between KFold and Bootstrap is:
+K-Fold keeps in consideration of balance of classes
+Bootstrap shuufles and splits the records independent of balance of classes 
+which makes difference while solving for classification problem
